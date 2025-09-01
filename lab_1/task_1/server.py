bd779b82c0af33af848e0daa5aaadd950d25740d
KlimVladMaks <vkm.blog@yandex.ru>
2025-09-01 17:25:13 +0300

    Commit 2

--- lab_1/task_1/server.py
+++ lab_1/task_1/server.py
@@ -3,7 +3,7 @@ import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_socket.bind(('localhost', 8080))
-print("UDP сервер запущен на порту 8080...")
+print("UDP-сервер запущен на порту 8080...")
 
 while True:
     data, client_address = server_socket.recvfrom(1024)
