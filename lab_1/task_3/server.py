92951aa63d9aab157f35636b6abba50c0b46b9b6
KlimVladMaks <vkm.blog@yandex.ru>
2025-09-02 12:20:47 +0300

    Commit 5

--- lab_1/task_3/server.py
+++ lab_1/task_3/server.py
@@ -6,6 +6,7 @@ def main():
     port = 8080
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     try:
         server_socket.bind((host, port))
